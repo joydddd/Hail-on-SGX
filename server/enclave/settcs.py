262b5dcf46fbd53c783964180ab0f6806760a49f
jonahrosenblum <jonaher@sgx.eecs.umich.edu>
2021-12-28 00:50:34 -0500

    add script to make process that sets TCS in enclave config

---
+++ server/enclave/settcs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import multiprocessing
+import shutil
+import os
+
+with open('gwas_enc.conf', 'r') as fread, open('gwas_enc.conf.temp', 'w') as fwrite:
+    for line in fread:
+        if 'NumTCS' in line:
+            line = 'NumTCS=' + str(multiprocessing.cpu_count() + 1) + '\n'
+        fwrite.write(line)
+
+shutil.copyfile('gwas_enc.conf.temp', 'gwas_enc.conf')
+os.remove('gwas_enc.conf.temp')
