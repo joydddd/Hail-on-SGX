84b35c14239879918faa590940d1fb810f49381b
jonaherosenblum@gmail.com <ubuntu@ip-172-31-18-203.ec2.internal>
2022-01-03 03:12:36 +0000

    add username option in install reqs

--- archive/client_dataprocess/install_reqs.sh
+++ archive/client_dataprocess/install_reqs.sh
@@ -3,3 +3,4 @@ sudo apt install python3-pip
 sudo apt install python3.8-venv
 sudo apt install default-jre
 pip3 install -r requirements.txt
+git config --global user.name "jonaherosenblum@gmail.com"
