968eb8ced2616d1b711a31ecdbb4c0e2f668c860
Jonah Rosenblum <jonaherosenblum@gmail.com>
2022-01-03 03:15:20 +0000

    add python venv to install script

--- archive/client_dataprocess/install_reqs.sh
+++ archive/client_dataprocess/install_reqs.sh
@@ -2,5 +2,8 @@ sudo apt update
 sudo apt install python3-pip
 sudo apt install python3.8-venv
 sudo apt install default-jre
+python3 -m venv env
+source env/bin/activate
 pip3 install -r requirements.txt
-git config --global user.name "jonaherosenblum@gmail.com"
+git config --global user.name "Jonah Rosenblum"
+git config --global user.email jonaherosenblum@gmail.com
