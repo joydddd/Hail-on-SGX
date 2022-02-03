f3de87af63d330362797860ad24cba92f7a9d3f9
joydddd <joydong@umich.edu>
2022-02-03 16:31:54 -0500

    Add simluation mode

--- compute_server/host/src/main.cpp
+++ compute_server/host/src/main.cpp
@@ -6,14 +6,19 @@
 #include <iostream>
 #include <stdlib.h>
 
-
+EncMode ComputeServer::enc_mode = NA;
 int main(int argc, char const *argv[]) {
 
-    if (argc != 2) {
-        // server ran incorrectly
-        std::cout << "Usage: <EXE> [config_file]" << std::endl;
+    if (argc == 2)
+        ComputeServer::enc_mode = sgx;
+    if (argc == 3 && !strcmp(argv[2], "--simulate"))
+        ComputeServer::enc_mode = simulate;
+    if (argc == 3 && !strcmp(argv[2], "--debug"))
+        ComputeServer::enc_mode = debug;
+    if (ComputeServer::enc_mode == NA){
+        std::cout << "Usage: <EXE> [config_file] (--simlation)" << std::endl;
         return 1;
-    } 
+    }
 
     // initialize our server with the given port, and run it forever
     ComputeServer::get_instance(argv[1]);
