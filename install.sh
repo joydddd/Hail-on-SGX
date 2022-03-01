c3d94eff1d7209b4183c0f46c169345ee671f533
Jonah Rosenblum <jonaherosenblum@gmail.com>
2022-03-01 13:56:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 sudo apt-get update -y
+sudo apt-get install make
+sudo apt-get install g++
 sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils
 sudo apt-get install libboost-all-dev -y
