a640db186e3966ec5bebcff01cf78ce085e573f7
Ubuntu <jonahrosenblum@test-sgx.02wptkqhro3evdei15l0on5i5g.bx.internal.cloudapp.net>
2022-03-02 05:42:56 +0000

    update install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo apt-get update -y
-sudo apt-get install make
-sudo apt-get install g++
-sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils
+sudo apt-get install make -y
+sudo apt-get install g++ -y
+sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils -y
+sudo apt-get install python -y
 sudo apt-get install libboost-all-dev -y
