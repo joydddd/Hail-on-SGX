edfa01fc9d699a93abb1f5d9b6a1e8090f6cdaa0
Ubuntu <jonahrosenblum@open-group.ystrnyifkh4ufghd1crystnawf.bx.internal.cloudapp.net>
2022-03-03 04:38:23 +0000

    configure the apt repo for openenclave

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu focal main' | sudo tee /etc/apt/sources.list.d/intel-sgx.list
+wget -qO - https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | sudo apt-key add -
+
+echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-10 main" | sudo tee /etc/apt/sources.list.d/llvm-toolchain-focal-10.list
+wget -qO - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+
+echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/20.04/prod focal main" | sudo tee /etc/apt/sources.list.d/msprod.list
+wget -qO - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+sudo apt update
 sudo apt-get update -y
 sudo apt-get install make -y
 sudo apt-get install g++ -y
@@ -5,3 +15,6 @@ sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils -y
 sudo apt-get install python -y
 sudo apt-get install libcurl4-openssl-dev -y
 sudo apt-get install libboost-all-dev -y
+sudo apt-get install clang-10 libssl-dev gdb libsgx-enclave-common libsgx-quote-ex libprotobuf17 libsgx-dcap-ql libsgx-dcap-ql-dev az-dcap-client open-enclave -y
+
+source /opt/openenclave/share/openenclave/openenclaverc
