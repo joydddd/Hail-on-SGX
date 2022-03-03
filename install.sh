8fe53e95960078115324ad909d47594c132a2965
Ubuntu <jonahrosenblum@hope-ips-work.hfgd4okqfj0u1pa1j54yqjpecd.bx.internal.cloudapp.net>
2022-03-03 03:35:55 +0000

    Add libcurl to install script

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ sudo apt-get install make -y
 sudo apt-get install g++ -y
 sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils -y
 sudo apt-get install python -y
+sudo apt-get install libcurl4-openssl-dev -y
 sudo apt-get install libboost-all-dev -y
