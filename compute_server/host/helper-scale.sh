01871b0ffca805d76bf6f629404d25952fee222d
Ubuntu <azureuser@sgx-register-client.h1fp5hwfutzulapupdevcxafce.dx.internal.cloudapp.net>
2022-08-30 21:36:06 +0000

    Update helper script for experiment

--- compute_server/host/helper-scale.sh
+++ compute_server/host/helper-scale.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./gwashost compute_server_config.json > output-log-64.txt
-./gwashost compute_server_configlinear.json > output-lin-64.txt
+./gwashost compute_server_config.json > output-log-128.txt
+./gwashost compute_server_configlinear.json > output-lin-128.txt
