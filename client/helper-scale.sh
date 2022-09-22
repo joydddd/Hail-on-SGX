b1c33ed19dd0d249d4c44225491532f8dbe26ac4
Ubuntu <azureuser@sgx-register-client.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2022-09-22 19:19:19 +0000

    More config changes for large scale test

--- client/helper-scale.sh
+++ client/helper-scale.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-./bin/client client_config.json
-./bin/client client_config.json
+./bin/client client_config1000.json
+./bin/client client_config1000.json
+./bin/client client_config5000.json
+./bin/client client_config5000.json
+./bin/client client_config10000.json
+./bin/client client_config10000.json
--- compute_server/host/helper-scale.sh
+++ compute_server/host/helper-scale.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-./gwashost compute_server_config.json > output-log-256.txt
-./gwashost compute_server_configlinear.json > output-lin-256.txt
+./gwashost compute_server_config1000log.json
+./gwashost compute_server_config1000lin.json
+./gwashost compute_server_config5000log.json
+./gwashost compute_server_config5000lin.json
+./gwashost compute_server_config10000log.json
+./gwashost compute_server_config10000lin.json
