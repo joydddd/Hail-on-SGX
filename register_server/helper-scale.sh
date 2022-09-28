a2cb79156d4883d1f4629eba547c3babacda21eb
Ubuntu <azureuser@sgx-register-client.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2022-09-28 18:03:28 +0000

    Update scale script for 256 testing

--- register_server/helper-scale.sh
+++ register_server/helper-scale.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 for _ in {1..3}
 do
-    # echo "Log 1000"
-    # ./bin/register_server register_config.json
-    # echo "Lin 1000"
-    # ./bin/register_server register_config.json
-    # echo "Log 5000"
-    # ./bin/register_server register_config.json
-    # echo "Lin 5000"
-    # ./bin/register_server register_config.json
+    echo "Log 1000"
+    ./bin/register_server register_config.json
+    echo "Lin 1000"
+    ./bin/register_server register_config.json
+    echo "Log 5000"
+    ./bin/register_server register_config.json
+    echo "Lin 5000"
+    ./bin/register_server register_config.json
     echo "Log 10000"
     ./bin/register_server register_config.json
     echo "Lin 10000"
