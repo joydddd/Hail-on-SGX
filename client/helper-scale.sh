df96dd44b3c0cccd880fcdfefcf76b9b28503914
Ubuntu <azureuser@sgx-register-client.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2022-09-30 04:55:06 +0000

    Revert helper scale

--- client/helper-scale.sh
+++ client/helper-scale.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 for _ in {1..3}
 do
-    echo "Log 1000"
-    ./bin/client client_config1000.json
-    echo "Lin 1000"
-    ./bin/client client_config1000.json
-    echo "Log 5000"
-    ./bin/client client_config5000.json
+    # echo "Log 1000"
+    # ./bin/client client_config1000.json
+    # echo "Lin 1000"
+    # ./bin/client client_config1000.json
+    # echo "Log 5000"
+    # ./bin/client client_config5000.json
     echo "Lin 5000"
     ./bin/client client_config5000.json
-    echo "Log 10000"
-    ./bin/client client_config10000.json
-    echo "Lin 10000"
-    ./bin/client client_config10000.json
+    # echo "Log 10000"
+    # ./bin/client client_config10000.json
+    # echo "Lin 10000"
+    # ./bin/client client_config10000.json
 done
