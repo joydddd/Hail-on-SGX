e47a3681d7de35c0f402e8fa9e4f804e5827b9bb
Ubuntu <azureuser@sgx-register-client.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2022-12-12 17:11:29 +0000

    try w just log

--- compute_server/host/helper-scale.sh
+++ compute_server/host/helper-scale.sh
@@ -2,9 +2,9 @@
 for _ in {1..10}
 do
     ./gwashost compute_server_config1000log.json
-    ./gwashost compute_server_config1000lin.json
+    # ./gwashost compute_server_config1000lin.json
     ./gwashost compute_server_config5000log.json
-    ./gwashost compute_server_config5000lin.json
+    # ./gwashost compute_server_config5000lin.json
     ./gwashost compute_server_config10000log.json
-    ./gwashost compute_server_config10000lin.json
+    # ./gwashost compute_server_config10000lin.json
 done
--- register_server/helper-scale.sh
+++ register_server/helper-scale.sh
@@ -3,14 +3,14 @@ for _ in {1..10}
 do
     echo "Log 1000"
     ./bin/register_server register_config.json
-    echo "Lin 1000"
-    ./bin/register_server register_config.json
+    # echo "Lin 1000"
+    # ./bin/register_server register_config.json
     echo "Log 5000"
     ./bin/register_server register_config.json
-    echo "Lin 5000"
-    ./bin/register_server register_config.json
+    # echo "Lin 5000"
+    # ./bin/register_server register_config.json
     echo "Log 10000"
     ./bin/register_server register_config.json
-    echo "Lin 10000"
-    ./bin/register_server register_config.json
+    # echo "Lin 10000"
+    # ./bin/register_server register_config.json
 done
