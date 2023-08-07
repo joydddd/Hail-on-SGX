63aaaca07b641ccba3a3394c5e5d21b44d979a33
Ubuntu <azureuser@sgx-register-client.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2023-08-07 18:50:41 +0000

    update IPs

--- register_server/helper-scale.sh
+++ register_server/helper-scale.sh
@@ -2,15 +2,15 @@
 for _ in {1..10}
 do
     echo "Log 5000"
-    ./bin/register_server register_config.json >> scaling-5k-64-2.txt
+    ./bin/register_server register_config.json >> scaling-5k-64-3.txt
     echo "Lin 5000"
-    ./bin/register_server register_config.json >> scaling-5k-64-2.txt
+    ./bin/register_server register_config.json >> scaling-5k-64-3.txt
     echo "Log 10000"
-    ./bin/register_server register_config.json >> scaling-10k-64-2.txt
+    ./bin/register_server register_config.json >> scaling-10k-64-3.txt
     echo "Lin 10000"
-    ./bin/register_server register_config.json >> scaling-10k-64-2.txt
+    ./bin/register_server register_config.json >> scaling-10k-64-3.txt
     echo "Log 20000"
-    ./bin/register_server register_config.json >> scaling-20k-64-2.txt
+    ./bin/register_server register_config.json >> scaling-20k-64-3.txt
     echo "Lin 20000"
-    ./bin/register_server register_config.json >> scaling-20k-64-2.txt
+    ./bin/register_server register_config.json >> scaling-20k-64-3.txt
 done
