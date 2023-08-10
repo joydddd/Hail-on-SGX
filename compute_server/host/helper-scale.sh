50611fd770d1a235a3894d4ab8af4e20f02eb185
Ubuntu <azureuser@sgx-register-client1.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2023-08-10 19:47:13 +0000

    prepping for 4 client tests

--- compute_server/host/helper-scale.sh
+++ compute_server/host/helper-scale.sh
@@ -2,15 +2,15 @@
 for _ in {1..10}
 do
     echo "Log 5000"
-    ./gwashost compute_server_config5000log.json >> scaling-5k-64.txt
+    ./gwashost compute_server_config1250log.json >> scaling-5k-64.txt
     echo "Lin 5000"
-    ./gwashost compute_server_config5000lin.json >> scaling-5k-64.txt
+    ./gwashost compute_server_config1250lin.json >> scaling-5k-64.txt
     echo "Log 10000"
-    ./gwashost compute_server_config10000log.json >> scaling-10k-64.txt
+    ./gwashost compute_server_config2500log.json >> scaling-10k-64.txt
     echo "Lin 10000"
-    ./gwashost compute_server_config10000lin.json >> scaling-10k-64.txt
+    ./gwashost compute_server_config2500lin.json >> scaling-10k-64.txt
     echo "Log 20000"
-    ./gwashost compute_server_config20000log.json >> scaling-20k-64.txt
+    ./gwashost compute_server_config5000log.json >> scaling-20k-64.txt
     echo "Lin 20000"
-    ./gwashost compute_server_config20000lin.json >> scaling-20k-64.txt
+    ./gwashost compute_server_config5000lin.json >> scaling-20k-64.txt
 done
--- data_processing/generate_alleles.py
+++ data_processing/generate_alleles.py
@@ -13,7 +13,7 @@ rand_size = 100003
 
 rands = np.random.random(rand_size)
 
-ALLELE_COUNT =  1000000
+ALLELE_COUNT =  4057178 
 CLIENT_COUNT = 5000 if len(sys.argv) != 2 else int(sys.argv[1])
 print(CLIENT_COUNT)
 NUM_PROCS = multiprocessing.cpu_count()
