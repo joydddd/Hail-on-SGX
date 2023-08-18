b1fe2475516b51b8034b08f1ea80a1cf37e4dea5
Ubuntu <azureuser@sgx-register-client1.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2023-08-18 05:22:40 +0000

    good enough for now

--- register_server/helper-scale.sh
+++ register_server/helper-scale.sh
@@ -2,15 +2,15 @@
 for _ in {1..5}
 do
     echo "Log 5000"
-    ./bin/register_server register_config.json >> scaling-5k-64-32.txt
+    ./bin/register_server register_config.json >> scaling-5k-32.txt
     echo "Lin 5000"
-    ./bin/register_server register_config.json >> scaling-5k-64-32.txt
+    ./bin/register_server register_config.json >> scaling-5k-32.txt
     echo "Log 10000"
-    ./bin/register_server register_config.json >> scaling-10k-64-32.txt
+    ./bin/register_server register_config.json >> scaling-10k-32.txt
     echo "Lin 10000"
-    ./bin/register_server register_config.json >> scaling-10k-64-32.txt
+    ./bin/register_server register_config.json >> scaling-10k-32.txt
     echo "Log 20000"
-    ./bin/register_server register_config.json >> scaling-20k-64-32.txt
+    ./bin/register_server register_config.json >> scaling-20k-32.txt
     echo "Lin 20000"
-    ./bin/register_server register_config.json >> scaling-20k-64-32.txt
+    ./bin/register_server register_config.json >> scaling-20k-32.txt
 done
