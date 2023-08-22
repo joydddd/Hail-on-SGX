23a0b75f04ce734e4b1015f47388ff9c3b4d8cb1
Ubuntu <azureuser@sgx-register-client1.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2023-08-22 03:30:58 +0000

    done w ratio?

--- client/helper-scale.sh
+++ client/helper-scale.sh
@@ -2,15 +2,15 @@
 for _ in {1..5}
 do
     echo "Log 5000"
-    ./bin/client client_config1250.json >> scaling-5k-2:1.txt
+    ./bin/client client_config1250.json >> scaling-5k-4:1.txt
     echo "Lin 5000"
-    ./bin/client client_config1250.json >> scaling-5k-2:1.txt
+    ./bin/client client_config1250.json >> scaling-5k-4:1.txt
     echo "Log 10000"
-    ./bin/client client_config2500.json >> scaling-10k-2:1.txt
+    ./bin/client client_config2500.json >> scaling-10k-4:1.txt
     echo "Lin 10000"
-    ./bin/client client_config2500.json >> scaling-10k-2:1.txt
+    ./bin/client client_config2500.json >> scaling-10k-4:1.txt
     echo "Log 20000"
-    ./bin/client client_config5000.json >> scaling-20k-2:1.txt
+    ./bin/client client_config5000.json >> scaling-20k-4:1.txt
     echo "Lin 20000"
-    ./bin/client client_config5000.json>> scaling-20k-2:1.txt
+    ./bin/client client_config5000.json>> scaling-20k-4:1.txt
 done
--- register_server/helper-scale.sh
+++ register_server/helper-scale.sh
@@ -2,15 +2,15 @@
 for _ in {1..5}
 do
     echo "Log 5000"
-    ./bin/register_server register_config.json >> scaling-5k-2:1.txt
+    ./bin/register_server register_config.json >> scaling-5k-4:1.txt
     echo "Lin 5000"
-    ./bin/register_server register_config.json >> scaling-5k-2:1.txt
+    ./bin/register_server register_config.json >> scaling-5k-4:1.txt
     echo "Log 10000"
-    ./bin/register_server register_config.json >> scaling-10k-2:1.txt
+    ./bin/register_server register_config.json >> scaling-10k-4:1.txt
     echo "Lin 10000"
-    ./bin/register_server register_config.json >> scaling-10k-2:1.txt
+    ./bin/register_server register_config.json >> scaling-10k-4:1.txt
     echo "Log 20000"
-    ./bin/register_server register_config.json >> scaling-20k-2:1.txt
+    ./bin/register_server register_config.json >> scaling-20k-4:1.txt
     echo "Lin 20000"
-    ./bin/register_server register_config.json >> scaling-20k-2:1.txt
+    ./bin/register_server register_config.json >> scaling-20k-4:1.txt
 done
