1ee668d4bbf8a645668b95a1f07405838c48bb1c
Ubuntu <azureuser@sgx-register-client1.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2023-08-23 03:02:07 +0000

    fear leads to anger. anger leads to hate.

--- register_server/helper-scale.sh
+++ register_server/helper-scale.sh
@@ -2,15 +2,35 @@
 for _ in {1..5}
 do
     echo "Log 5000"
-    ./bin/register_server register_config.json >> scaling-5k-4:1.txt
+    ./bin/register_server register_config.json >> scaling-5k-128-2.txt
+done
+
+for _ in {1..5}
+do
     echo "Lin 5000"
-    ./bin/register_server register_config.json >> scaling-5k-4:1.txt
+    ./bin/register_server register_config.json >> scaling-5k-128-2.txt
+done
+
+for _ in {1..5}
+do
     echo "Log 10000"
-    ./bin/register_server register_config.json >> scaling-10k-4:1.txt
+    ./bin/register_server register_config.json >> scaling-10k-128-2.txt
+done
+
+for _ in {1..5}
+do
     echo "Lin 10000"
-    ./bin/register_server register_config.json >> scaling-10k-4:1.txt
+    ./bin/register_server register_config.json >> scaling-10k-128-2.txt
+done
+
+for _ in {1..5}
+do
     echo "Log 20000"
-    ./bin/register_server register_config.json >> scaling-20k-4:1.txt
+    ./bin/register_server register_config.json >> scaling-20k-128-2.txt
+done
+
+for _ in {1..5}
+do
     echo "Lin 20000"
-    ./bin/register_server register_config.json >> scaling-20k-4:1.txt
+    ./bin/register_server register_config.json >> scaling-20k-128-2.txt
 done
