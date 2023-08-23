4b01effbb354adfcc5aca83a1533a0ce867d2641
Ubuntu <azureuser@sgx-register-client1.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2023-08-23 18:21:41 +0000

    8 core machines here we come :D

---
+++ compute_server/host/helper-scale3.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+for _ in {1..5}
+do
+    echo "log-2 5000"
+    ./gwashost compute_server_config1250log-3.json >> scaling-5k-64-3.txt
+done
+
+for _ in {1..5}
+do
+    echo "Lin 5000"
+    ./gwashost compute_server_config1250lin-3.json >> scaling-5k-64-3.txt
+done
+
+for _ in {1..5}
+do
+    echo "log-2 10000"
+    ./gwashost compute_server_config2500log-3.json >> scaling-10k-64-3.txt
+done
+
+for _ in {1..5}
+do
+     echo "Lin 10000"
+    ./gwashost compute_server_config2500lin-3.json >> scaling-10k-64-3.txt
+done
+
+for _ in {1..5}
+do
+    echo "log-2 20000"
+    ./gwashost compute_server_config5000log-3.json >> scaling-20k-64-3.txt
+done
+
+for _ in {1..5}
+do
+    echo "Lin 20000"
+    ./gwashost compute_server_config5000lin-3.json >> scaling-20k-64-3.txt
+done
---
+++ compute_server/host/helper-scale4.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+for _ in {1..5}
+do
+    echo "log-2 5000"
+    ./gwashost compute_server_config1250log-4.json >> scaling-5k-64-4.txt
+done
+
+for _ in {1..5}
+do
+    echo "Lin 5000"
+    ./gwashost compute_server_config1250lin-4.json >> scaling-5k-64-4.txt
+done
+
+for _ in {1..5}
+do
+    echo "log-2 10000"
+    ./gwashost compute_server_config2500log-4.json >> scaling-10k-64-4.txt
+done
+
+for _ in {1..5}
+do
+     echo "Lin 10000"
+    ./gwashost compute_server_config2500lin-4.json >> scaling-10k-64-4.txt
+done
+
+for _ in {1..5}
+do
+    echo "log-2 20000"
+    ./gwashost compute_server_config5000log-4.json >> scaling-20k-64-4.txt
+done
+
+for _ in {1..5}
+do
+    echo "Lin 20000"
+    ./gwashost compute_server_config5000lin-4.json >> scaling-20k-64-4.txt
+done
