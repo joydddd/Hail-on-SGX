48ac4c412152c0989ca5f6b348fdfa225ee60deb
Ubuntu <azureuser@sgx-register-client.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2023-09-08 16:43:23 +0000

    let's find the last bug!

---
+++ compute_server/host/helper-scale7.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+for _ in {1..15}
+do
+    echo "log-2 5000"
+    ./gwashost configs/compute_server_config1250log-7.json >> scaling-5k-64-7.txt
+done
+
+for _ in {1..15}
+do
+    echo "Lin 5000"
+    ./gwashost configs/compute_server_config1250lin-7.json >> scaling-5k-64-7.txt
+done
+
+for _ in {1..15}
+do
+    echo "log-2 10000"
+    ./gwashost configs/compute_server_config2500log-7.json >> scaling-10k-64-7.txt
+done
+
+for _ in {1..15}
+do
+     echo "Lin 10000"
+    ./gwashost configs/compute_server_config2500lin-7.json >> scaling-10k-64-7.txt
+done
+
+for _ in {1..15}
+do
+    echo "log-2 20000"
+    ./gwashost configs/compute_server_config5000log-7.json >> scaling-20k-64-7.txt
+done
+
+for _ in {1..15}
+do
+    echo "Lin 20000"
+    ./gwashost configs/compute_server_config5000lin-7.json >> scaling-20k-64-7.txt
+done
+
+for _ in {1..15}
+do
+    echo "Log 20000"
+    ./gwashost configs/compute_server_config5000log-famhe-7.json >> scaling-20k-64.txt
+done
+
+for _ in {1..15}
+do
+    echo "Lin 20000"
+    ./gwashost configs/compute_server_config5000lin-famhe-7.json >> scaling-20k-64.txt
+done
+
+for _ in {1..15}
+do
+    echo "Log 20000"
+    ./gwashost configs/compute_server_config5000log-oblivious-7.json >> scaling-20k-64.txt
+done
+
+for _ in {1..15}
+do
+    echo "Lin 20000"
+    ./gwashost configs/compute_server_config5000lin-oblivious-7.json >> scaling-20k-64.txt
+done
--- register_server/helper-scale.sh
+++ register_server/helper-scale.sh
@@ -1,59 +1,59 @@
 #!/bin/bash
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Log 5000"
     ./bin/register_server register_config.json >> scaling-5k-testing.txt
 done
 
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Lin 5000"
     ./bin/register_server register_config.json >> scaling-5k-testing.txt
 done
 
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Log 10000"
     ./bin/register_server register_config.json >> scaling-10k-testing.txt
 done
 
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Lin 10000"
     ./bin/register_server register_config.json >> scaling-10k-testing.txt
 done
 
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Log 20000"
     ./bin/register_server register_config.json >> scaling-20k-testing.txt
 done
 
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Lin 20000"
     ./bin/register_server register_config.json >> scaling-20k-testing.txt
 done
 
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Log 20000"
     ./bin/register_server register_config.json >> scaling-20k-famhe-testing.txt
 done
 
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Lin 20000"
     ./bin/register_server register_config.json >> scaling-20k-famhe-testing.txt
 done
 
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Log 20000"
     ./bin/register_server register_config.json >> scaling-20k-oblivious-testing.txt
 done
 
-for _ in {1..5}
+for _ in {1..15}
 do
     echo "Lin 20000"
     ./bin/register_server register_config.json >> scaling-20k-oblivious-testing.txt
