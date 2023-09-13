2f19fccb4c1753590552691acdad57b9b1f68b65
Ubuntu <azureuser@sgx-register-client.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2023-09-13 19:20:27 +0000

    ok enough spam

--- shared/src/socket_send.cpp
+++ shared/src/socket_send.cpp
@@ -88,7 +88,6 @@ int send_message(const char *hostname, int port, const char *message, const int 
 				sock = -1;
 				continue;
 			}
-			std::cout << "b4 connect" << std::endl;
 			if (connect(sock, addr->ai_addr, addr->ai_addrlen) == 0) {
 				break;
 			}
@@ -96,9 +95,7 @@ int send_message(const char *hostname, int port, const char *message, const int 
 			close(sock);
 			sock = -1;
 		}
-		std::cout << "after connect" << std::endl;
 		freeaddrinfo(addrs);
-		std::cout << "after freeaddrinfo" << std::endl;
 		if (sock == -1) {
 			char buffer[ 256 ];
 			char * errorMsg = strerror_r( errno, buffer, 256 ); // GNU-specific version, Linux default
@@ -106,12 +103,13 @@ int send_message(const char *hostname, int port, const char *message, const int 
 			throw std::runtime_error("Failed to connect\n");
 		}
 	}
-	std::cout << "about to send" << std::endl;
+	if (sock == -1 || !message || !message_length) {
+		throw std::runtime_error("Missed something on send.");
+	}
 	// Send message to remote server
 	if (send(sock, message, message_length, 0) == -1) {
 		throw std::runtime_error("Hostname: " + std::string(hostname) + " error sending on stream socket");
 	}
-	std::cout << "after send" << std::endl;
 
 	return sock;
 }
