99d451769bab2c511a6a4fab89b2d91731433061
Ubuntu <azureuser@sgx-register-client1.zhrvpprbnu1unkhsoq0wdjux5g.bx.internal.cloudapp.net>
2023-09-20 19:52:21 +0000

    idk anymore man

--- register_server/helper-scale.sh
+++ register_server/helper-scale.sh
@@ -2,59 +2,59 @@
 for _ in {1..7}
 do
     echo "Log 5000"
-    ./bin/register_server register_config.json >> scaling-5k-omega.txt
+    ./bin/register_server register_config.json >> scaling-5k-omega-1.txt
 done
 
 for _ in {1..7}
 do
     echo "Lin 5000"
-    ./bin/register_server register_config.json >> scaling-5k-omega.txt
+    ./bin/register_server register_config.json >> scaling-5k-omega-1.txt
 done
 
 for _ in {1..7}
 do
     echo "Log 10000"
-    ./bin/register_server register_config.json >> scaling-10k-omega.txt
+    ./bin/register_server register_config.json >> scaling-10k-omega-1.txt
 done
 
 for _ in {1..7}
 do
     echo "Lin 10000"
-    ./bin/register_server register_config.json >> scaling-10k-omega.txt
+    ./bin/register_server register_config.json >> scaling-10k-omega-1.txt
 done
 
 for _ in {1..7}
 do
     echo "Log 20000"
-    ./bin/register_server register_config.json >> scaling-20k-omega.txt
+    ./bin/register_server register_config.json >> scaling-20k-omega-1.txt
 done
 
 for _ in {1..7}
 do
     echo "Lin 20000"
-    ./bin/register_server register_config.json >> scaling-20k-omega.txt
+    ./bin/register_server register_config.json >> scaling-20k-omega-1.txt
 done
 
 for _ in {1..7}
 do
     echo "Log 20000"
-    ./bin/register_server register_config.json >> scaling-20k-famhe-omega.txt
+    ./bin/register_server register_config.json >> scaling-20k-famhe-omega-1.txt
 done
 
 for _ in {1..7}
 do
     echo "Lin 20000"
-    ./bin/register_server register_config.json >> scaling-20k-famhe-omega.txt
+    ./bin/register_server register_config.json >> scaling-20k-famhe-omega-1.txt
 done
 
 for _ in {1..7}
 do
     echo "Log 20000"
-    ./bin/register_server register_config.json >> scaling-20k-oblivious-omega.txt
+    ./bin/register_server register_config.json >> scaling-20k-oblivious-omega-1.txt
 done
 
 for _ in {1..7}
 do
     echo "Lin 20000"
-    ./bin/register_server register_config.json >> scaling-20k-oblivious-omega.txt
+    ./bin/register_server register_config.json >> scaling-20k-oblivious-omega-1.txt
 done
