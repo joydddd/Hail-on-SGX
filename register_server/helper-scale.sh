3ce292789a0133a64572710b692d0da2b44eacb5
Ubuntu <azureuser@sgx-register-client1.ris33wtj2zyerndyj1z5xfyyfh.dx.internal.cloudapp.net>
2023-09-27 18:35:53 +0000

    hnngg

--- register_server/helper-scale.sh
+++ register_server/helper-scale.sh
@@ -3,59 +3,59 @@
 for _ in {1..5}
 do
     echo "Log 5000"
-    ./bin/register_server register_config.json >> scaling-5k-256-LAN.txt
+    ./bin/register_server register_config.json >> scaling-5k-512-LAN.txt
 done
 
 for _ in {1..5}
 do
     echo "Lin 5000"
-    ./bin/register_server register_config.json >> scaling-5k-256-LAN.txt
+    ./bin/register_server register_config.json >> scaling-5k-512-LAN.txt
 done
 
 for _ in {1..5}
 do
     echo "Log 10000"
-    ./bin/register_server register_config.json >> scaling-10k-256-LAN.txt
+    ./bin/register_server register_config.json >> scaling-10k-512-LAN.txt
 done
 
 for _ in {1..5}
 do
     echo "Lin 10000"
-    ./bin/register_server register_config.json >> scaling-10k-256-LAN.txt
+    ./bin/register_server register_config.json >> scaling-10k-512-LAN.txt
 done
 
 for _ in {1..5}
 do
     echo "Log 20000"
-    ./bin/register_server register_config.json >> scaling-20k-256-LAN.txt
+    ./bin/register_server register_config.json >> scaling-20k-512-LAN.txt
 done
 
 for _ in {1..5}
 do
     echo "Lin 20000"
-    ./bin/register_server register_config.json >> scaling-20k-256-LAN.txt
+    ./bin/register_server register_config.json >> scaling-20k-512-LAN.txt
 done
 
 # for _ in {1..5}
 # do
 #     echo "Log 20000"
-#     ./bin/register_server register_config.json >> scaling-20k-famhe-256-LAN.txt
+#     ./bin/register_server register_config.json >> scaling-20k-famhe-512-LAN.txt
 # done
 
 # for _ in {1..5}
 # do
 #     echo "Lin 20000"
-#     ./bin/register_server register_config.json >> scaling-20k-famhe-256-LAN.txt
+#     ./bin/register_server register_config.json >> scaling-20k-famhe-512-LAN.txt
 # done
 
 # for _ in {1..5}
 # do
 #     echo "Log 20000"
-#     ./bin/register_server register_config.json >> scaling-20k-oblivious-256-LAN.txt
+#     ./bin/register_server register_config.json >> scaling-20k-oblivious-512-LAN.txt
 # done
 
 # for _ in {1..5}
 # do
 #     echo "Lin 20000"
-#     ./bin/register_server register_config.json >> scaling-20k-oblivious-256-LAN.txt
+#     ./bin/register_server register_config.json >> scaling-20k-oblivious-512-LAN.txt
 # done
